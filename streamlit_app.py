e75c5fbde97cc33230a72f3bcbd0ec04c90c2b71
tiram1su <122186897+tiram1su@users.noreply.github.com>
2023-07-22 21:24:02 -0400

    Update streamlit_app.py
    Updated the syntax, as emoji was outside the ' ' on line 4

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
 
-streamlit.header(🥣 'Breakfast Menu')
+streamlit.header('🥣 Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
