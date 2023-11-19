1cdd0c6663546d6013bb83610857d3f57f568de2
tiram1su <122186897+tiram1su@users.noreply.github.com>
2023-11-19 16:46:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,11 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.dataframe(my_fruit_list)
 
-# Let's put a pick list here so they can pick the fruit they want to include 
+#Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page.
+#Display the table on the page.
 
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
@@ -32,6 +32,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #Not sure what this does
 streamlit.dataframe(fruityvice_normalized)
+
+#Don't run anything past here while we troubleshoot
+streamlit.stop()
 
 #Import the Snowflake Connector
 import snowflake.connector
